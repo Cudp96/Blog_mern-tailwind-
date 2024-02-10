37fed538bb655e8e6147c414c1f3119f2c7d33b0
Sudeep <sudeepstha.96@gmail.com>
2024-02-10 22:13:15 +0545

    feat : Frontend and Backend installation and db as well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Navbar from "./components/Header";
 
 const App = () => {
   return (
-    <div className='text-slate-700 font-bold'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
