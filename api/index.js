0cf0a3e15d35184a6e9c1f6e3e5c35d3ea3512b6
Sudeep <sudeepstha.96@gmail.com>
2024-02-13 23:07:04 +0545

    feat: update functionality done for backend.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -20,6 +21,8 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
 });
+
+app.use(cookieParser());
 
 app.use(express.json());
 
