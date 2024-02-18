a40104cd76db0573dbfd211f04a39e79c9635b61
Sudeep <sudeepstha.96@gmail.com>
2024-02-18 15:08:30 +0545

    feat: post ui/ux made

--- api/controllers/post.contoller.js
+++ api/controllers/post.contoller.js
@@ -33,47 +33,40 @@ export const getPosts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-
-    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
       ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
-      ...(
-        req.query.searchTerm && {
-          $or: [
-            { title: { regex: req.query.searchTerm, $options: "i" } },
-            {
-              content: {
-                regex: req.query.searchTerm,
-                $options: "i",
-              },
-            },
-          ],
-        }
-      )
-        .sort({ updatedAt: sortDirection })
-        .skip(startIndex)
-        .limit(limit),
-    });
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
 
-    const totalPost = await Post.countDocuments();
+    const totalPosts = await Post.countDocuments();
 
     const now = new Date();
-    const oneMonthago = new Date(
+
+    const oneMonthAgo = new Date(
       now.getFullYear(),
       now.getMonth() - 1,
       now.getDate()
     );
+
     const lastMonthPosts = await Post.countDocuments({
-      createdAt: {
-        $gte: oneMonthago,
-      },
+      createdAt: { $gte: oneMonthAgo },
     });
+
     res.status(200).json({
       posts,
-      totalPost,
+      totalPosts,
       lastMonthPosts,
     });
   } catch (error) {
